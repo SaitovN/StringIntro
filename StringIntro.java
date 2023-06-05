a3837ecd8cbbe764260d84d166fa6510866e391b
SaitovN <nuriksaitov03@gmailcom>
2023-06-05 16:49:41 +0600

    Create StringIntro.java

---
+++ StringIntro.java
@@ -0,0 +1,15 @@
+package academy.devonline.java.basic.section01_setup.section06_setup;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class StringIntro {
+    public static void main(String[] args) {
+        var hel = "hello";
+        System.out.println(hel);
+        System.out.println("Hello");
+        char [] array = {'h','e','l','l','o'};
+            System.out.print(Arrays.toString(array));
+
+
+    }}
